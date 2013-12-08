e9f87726c286c1e6db5e6dcf58162d89a60a49dc
Michael West <michaellwest@gmail.com>
2013-12-08 11:51:42 -0600

    Added scripts and controls.

---
+++ Website/sitecore/shell/Applications/Content Manager/Dialogs/EditHtmlEx/EditHtmlEx.js
@@ -0,0 +1,37 @@
+(function($){
+    $('head').append("<style>div[id$=RibbonPanel],textarea[id$=Html] { display: none; } #CodeEditor { width: 100%; height: 100%; position: initial; } </style>");
+ 
+    $(function() {
+        var html = $('textarea[id$=Html]');
+        var ce = $("<div id='CodeEditor' />");
+        html.after(ce);
+ 
+        var codeeditor = ace.edit(ce[0]);
+        codeeditor.setTheme("ace/theme/monokai");
+        codeeditor.session.setMode("ace/mode/html");
+        codeeditor.setShowPrintMargin(false);
+ 
+        codeeditor.session.setValue(html.val().trim());
+        codeeditor.session.on('change', function () {
+                html.val(codeeditor.session.getValue());
+        });
+
+        /*ace.config.loadModule("ace/ext/emmet", function () {
+            ace.require("ace/lib/net").loadScript("/sitecore/shell/Controls/Lib/ace/emmet-core/emmet.js", function () {
+                codeeditor.setOption("enableEmmet", true);
+            });
+ 
+            codeeditor.setOptions({
+                enableSnippets: true,
+                enableBasicAutocompletion: true
+            });              
+        });
+ 
+        ace.config.loadModule("ace/ext/language_tools", function (module) {
+            codeeditor.setOptions({
+                enableSnippets: true,
+                enableBasicAutocompletion: true
+            });
+        });*/
+    });
+}(jQuery));
