bc538e4a7553dc00e9fe29a00b942ec59ee2c070
Michael West <michaellwest@gmail.com>
2013-12-08 11:35:55 -0600

    Added initial required items and code.

---
+++ Sitecore.SharedSource/FieldTypeExtensions/RichTextEx.cs
@@ -0,0 +1,64 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Text;
+using Sitecore.Web;
+using Sitecore.Web.UI.Sheer;
+
+namespace Sitecore.SharedSource.FieldTypeExtensions
+{
+    public class RichTextEx : RichText
+    {
+        public override void HandleMessage(Message message)
+        {
+            Assert.ArgumentNotNull(message, "message");
+
+            if (message["id"] != ID) return;
+
+            string messageText;
+            if ((messageText = message.Name) == null)
+            {
+                return;
+            }
+
+            switch (messageText)
+            {
+                case "richtext:edithtml":
+                    Sitecore.Context.ClientPage.Start(this, "EditHtmlEx");
+                    return;
+            }
+
+            base.HandleMessage(message);
+        }
+
+        protected void EditHtmlEx(ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            if (Disabled) return;
+
+            if (args.IsPostBack)
+            {
+                if ((args.Result != null) && (args.Result != "undefined"))
+                {
+                    UpdateHtml(args);
+                }
+            }
+            else
+            {
+                var urlString =
+                    new UrlString(
+                        "/sitecore/shell/~/xaml/Sitecore.SharedSource.Shell.Applications.ContentEditor.Dialogs.EditHtmlEx.aspx");
+                var handle = new UrlHandle();
+                var str2 = Value;
+                if (str2 == "__#!$No value$!#__")
+                {
+                    str2 = string.Empty;
+                }
+                handle["html"] = str2;
+                handle.Add(urlString);
+                SheerResponse.ShowModalDialog(urlString.ToString(), "1000px", "500px", string.Empty, true);
+                args.WaitForPostBack();
+            }
+        }
+    }
+}
---
+++ Sitecore.SharedSource/Shell/Applications/ContentEditor/Dialogs/EditHtmlEx/EditHtmlExPage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web;
+using Sitecore.Web.UI.HtmlControls;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Web.UI.WebControls;
+using Sitecore.Web.UI.WebControls.Ribbons;
+using Sitecore.Web.UI.XamlSharp.Xaml;
+
+namespace Sitecore.SharedSource.Shell.Applications.ContentEditor.Dialogs.EditHtmlEx
+{
+    public class EditHtmlExPage : XamlMainControl
+    {
+        // Fields
+        protected TextBox Html;
+        protected Border RibbonPanel;
+
+        // Methods
+        protected virtual void Cancel_Click()
+        {
+            SheerResponse.CloseWindow();
+        }
+
+        protected virtual void OK_Click()
+        {
+            var text = Html.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                text = "__#!$No value$!#__";
+            }
+            SheerResponse.SetDialogValue(text);
+            SheerResponse.CloseWindow();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Assert.ArgumentNotNull(e, "e");
+            Assert.CanRunApplication("/sitecore/content/Applications/Content Editor/Dialogs/EditHtmlEx");
+            base.OnLoad(e);
+            if (!AjaxScriptManager.Current.IsEvent)
+            {
+                var handle = UrlHandle.Get();
+                Html.Text = handle["html"];
+                UpdateRibbon();
+            }
+        }
+
+        private void UpdateRibbon()
+        {
+            var ctl = new Ribbon {ID = "Ribbon", CommandContext = new CommandContext()};
+            var item =
+                Sitecore.Context.Database.GetItem(
+                    "/sitecore/content/Applications/Content Editor/Dialogs/EditHtmlEx/Ribbon");
+            ctl.CommandContext.RibbonSourceUri = item.Uri;
+            RibbonPanel.InnerHtml = HtmlUtil.RenderControl(ctl);
+        }
+    }
+}
