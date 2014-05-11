ac6a27fa11dba69e3318d624154dbbc4212392c3
Michael West <michaellwest@gmail.com>
2014-05-10 20:05:56 -0500

    Added a new WebControl to render the resources.

---
+++ Sitecore.SharedSource/Web/UI/ResourceWebControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Web.UI;
+using Sitecore.SharedSource.Data;
+using Sitecore.Web.UI;
+using Sitecore.Web.UI.HtmlControls;
+
+namespace Sitecore.SharedSource.Web.UI
+{
+    [ToolboxData("<{0}:ResourceWebControl runat=server></{0}:ResourceWebControl>")]
+    public class ResourceWebControl : WebControl
+    {
+        public ResourceType ResourceType { get; set; }
+
+        protected override void DoRender(HtmlTextWriter writer)
+        {
+            if (Sitecore.Context.Item == null || Sitecore.Context.Database == null)
+            {
+                return;
+            }
+
+            // Determine if the page has a resources.
+            var pageItem = Sitecore.Context.Item;
+            var resource = pageItem.Children.FirstOrDefault(x => x.TemplateID == TemplateIDs.ResourceReference);
+            if (resource == null)
+            {
+                return;
+            }
+
+            var results = ResourceRenderer.LoadResourceReference(resource);
+
+            if (ResourceType == ResourceType.Script && results.Any(x => x.Resource == ResourceType.Script))
+            {
+                writer.Write(ResourceRenderer.RenderResourceHtmlString(results, ResourceType.Script));
+            }
+            else if (ResourceType == ResourceType.Style && results.Any(x => x.Resource == ResourceType.Style))
+            {
+                writer.Write(ResourceRenderer.RenderResourceHtmlString(results, ResourceType.Style));
+            }
+        }
+    }
+}
