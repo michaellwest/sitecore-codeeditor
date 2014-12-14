045732b6b7ed07a5eaff4a4f56e0db06cfbc5229
Michael West <michaellwest@gmail.com>
2014-12-13 21:09:57 -0600

    Updated location of templates.

--- Sitecore.SharedSource/Configuration/ApplicationSettings.cs
+++ Sitecore.SharedSource/Configuration/ApplicationSettings.cs
@@ -1,5 +1,4 @@
 ﻿using Sitecore.Configuration;
-using Sitecore.Data;
 using Sitecore.SharedSource.Extensions;
 
 namespace Sitecore.SharedSource.Configuration
@@ -14,18 +13,13 @@ namespace Sitecore.SharedSource.Configuration
         {
             var settingsDb = Factory.GetDatabase(Settings.GetSetting("CodeEditor.Settings.Database", "master"));
 
-            // /sitecore/system/Modules/Code Editor/Settings/Dialog/All Users
-            var allusersId = ID.Parse("{9DAC8FA4-EE28-4D1F-842E-C107F51E2E6C}");
-            var allusers = settingsDb.GetItem(allusersId);
-
-            var width = Settings.GetSetting("CodeEditor.Dialog.Width").ToNumber(1000);
-            var height = Settings.GetSetting("CodeEditor.Dialog.Height").ToNumber(500);
+            var allusers = settingsDb.GetItem(ItemIDs.AllUsers);
 
             var settings = new DialogSettings
             {
                 Theme = allusers.Fields["Theme"].Value.ToLower(),
-                Width = allusers.Fields["Width"].Value.ToNumber(width),
-                Height = allusers.Fields["Height"].Value.ToNumber(height),
+                Width = allusers.Fields["Width"].Value.ToNumber(1000),
+                Height = allusers.Fields["Height"].Value.ToNumber(500),
                 FontSize = allusers.Fields["FontSize"].Value.ToNumber(12),
                 FontFamily = allusers.Fields["FontFamily"].Value
             };
--- Sitecore.SharedSource/TemplateIDs.cs
+++ Sitecore.SharedSource/TemplateIDs.cs
@@ -8,5 +8,6 @@ namespace Sitecore.SharedSource
         public static ID UnversionedCode = ID.Parse("{6228C4E1-782E-4CC9-A20B-FE047170DA00}");
         public static ID VersionedCode = ID.Parse("{D2B5EFB5-91FA-413A-AB11-F06F11BC9228}");
         public static ID Attachment = ID.Parse("{696DAB36-F6FA-4836-A4F8-5167DDE397E4}");
+        public static ID DialogSettings = ID.Parse("{4E034734-9440-4AB6-8BCD-1C393D8B781E}");
     }
 }
