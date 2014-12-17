598e9352f30921e28183fc231fcf383c8c09824f
Michael West <michaellwest@gmail.com>
2014-12-17 00:02:11 -0600

    Removed the EditHtmlEx control.
    Now reusing the code in the EditCode control.

--- Sitecore.SharedSource/Shell/Applications/ContentEditor/RichTextEx.cs
+++ Sitecore.SharedSource/Shell/Applications/ContentEditor/RichTextEx.cs
@@ -1,4 +1,6 @@
-﻿using Sitecore.Diagnostics;
+﻿using System;
+using Sitecore.Diagnostics;
+using Sitecore.SharedSource.Configuration;
 using Sitecore.Shell.Applications.ContentEditor;
 using Sitecore.Text;
 using Sitecore.Web;
@@ -47,16 +49,30 @@ namespace Sitecore.SharedSource.Shell.Applications.ContentEditor
             {
                 var urlString =
                     new UrlString(
-                        "/sitecore/shell/~/xaml/Sitecore.SharedSource.Shell.Applications.ContentEditor.Dialogs.EditHtmlEx.aspx");
+                        "/sitecore/shell/~/xaml/Sitecore.SharedSource.Shell.Applications.ContentEditor.Dialogs.EditCode.aspx");
+
+                var settings = ApplicationSettings.GetDialogSettings();
+
+                var parameters = WebUtil.ParseUrlParameters(Source);
+                if (parameters["mode"] != null)
+                {
+                    urlString.Append("mode", parameters["mode"]);
+                }
+                urlString.Append("theme", parameters["theme"] ?? settings.Theme);
+                urlString.Append("fontSize", parameters["fontSize"] ?? settings.FontSize.ToString());
+                urlString.Append("fontFamily", parameters["fontFamily"] ?? settings.FontFamily);
+
                 var handle = new UrlHandle();
                 var str2 = Value;
                 if (str2 == "__#!$No value$!#__")
                 {
                     str2 = string.Empty;
                 }
-                handle["html"] = str2;
+                handle["code"] = str2;
                 handle.Add(urlString);
-                SheerResponse.ShowModalDialog(urlString.ToString(), "1000px", "500px", string.Empty, true);
+
+                SheerResponse.ShowModalDialog(urlString.ToString(), settings.Width + "px", settings.Height + "px",
+                    String.Empty, true);
                 args.WaitForPostBack();
             }
         }
