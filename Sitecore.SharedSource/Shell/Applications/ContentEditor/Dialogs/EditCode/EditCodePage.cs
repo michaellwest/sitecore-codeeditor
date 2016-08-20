7dfab2028da4008441b7e66e01066ec61f9b55bf
Michael West <michaellwest@gmail.com>
2016-08-20 17:27:59 -0500

    #22 : Added theme and mode details to the dialog.

--- Sitecore.SharedSource/Shell/Applications/ContentEditor/Dialogs/EditCode/EditCodePage.cs
+++ Sitecore.SharedSource/Shell/Applications/ContentEditor/Dialogs/EditCode/EditCodePage.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Web.UI.WebControls;
 using Sitecore.Diagnostics;
 using Sitecore.Web;
@@ -12,6 +15,7 @@ namespace Sitecore.SharedSource.Shell.Applications.ContentEditor.Dialogs.EditCod
     {
         // Fields
         protected TextBox Code;
+        protected Label Details;
 
         // Methods
         protected virtual void Cancel_Click()
@@ -37,6 +41,24 @@ namespace Sitecore.SharedSource.Shell.Applications.ContentEditor.Dialogs.EditCod
             if (AjaxScriptManager.Current.IsEvent) return;
             var handle = UrlHandle.Get();
             Code.Text = handle["code"];
+
+            var details = new List<string>();
+            var mode = WebUtil.GetQueryString("mode");
+            if (!String.IsNullOrEmpty(mode))
+            {
+                details.Add($"Mode: {mode}");
+            }
+
+            var theme = WebUtil.GetQueryString("theme");
+            if (!String.IsNullOrEmpty(theme))
+            {
+                details.Add($"Theme: {theme}");
+            }
+
+            if (details.Any())
+            {
+                Details.Text = String.Join(" | ", details.ToArray());
+            }
         }
     }
 }
